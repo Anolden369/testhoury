fc4d0cfd6560cb6cb0321f65ad28c7c12e27c4b1
sanol <sanol@LAPTOP-2CDCG507>
2023-01-25 12:17:26 +0100

    Modification des erreurs

--- TP_0_Function/src/main/java/fr/eni/tp_0_function/TP_0_Function.java
+++ TP_0_Function/src/main/java/fr/eni/tp_0_function/TP_0_Function.java
@@ -4,6 +4,8 @@
 
 package fr.eni.tp_0_function;
 
+import java.util.Scanner;
+
 /**
  *
  * @author erwan
@@ -11,25 +13,32 @@ package fr.eni.tp_0_function;
 public class TP_0_Function {
 
     public static void main(String[] args) {
+        //t
         
         int intResultatSoustraction = simpleSoustraction(3,4);
         System.out.println(intResultatSoustraction);
 
-        tableDeMultiplication(5,13);
+        tableDeMultiplication(5,15);
         
-        String strChiffre = codeCesar("ABCD", 2);
-        System.out.println(strChiffre);
+        simpleAffichageSoustraction(3,4);
+        
+        simpleEntreeEtAffichage();
         
         int intBinaireConverti = conversionBaseDix(111);
         System.out.println(intBinaireConverti);
         
-        simpleAffichageSoustraction(3,4);
-
-        simpleEntreeEtAffichage();
+        String strChiffre = codeCesar("ABCD", 2);
+        System.out.println(strChiffre);
+       
     }
 
      public static void simpleEntreeEtAffichage()
     {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Quel nombre voulez-vous afficher : ");
+        int nombre = Integer.parseInt(input.next());
+        System.out.println(nombre);
+        
         /*
          * Vous devez simplement demander dans cette fonction à l'utilisateur 
          * de rentrer un nombre. Ensuite, à l'aide de println(), vous devrez
@@ -42,6 +51,8 @@ public class TP_0_Function {
     }
     public static int simpleSoustraction(int intA, int intB)
     {
+        int soustraction = intA - intB;
+        return(soustraction);
         /*
          * Vous devez simplement coder une fonction qui retourne le résultat
          * de la soustraction intA - intB.
@@ -52,6 +63,8 @@ public class TP_0_Function {
     
     public static void simpleAffichageSoustraction(int intA, int intB)
     {
+        int resultat = intA - intB;
+        System.out.println(resultat);
         /*
          * Vous devez simplement coder une fonction qui affiche le résultat
          * de la soustraction intA - intB.
@@ -62,6 +75,18 @@ public class TP_0_Function {
     
     public static void tableDeMultiplication(int intTable, int intNbLimite)
     {
+       if (intNbLimite > 0)
+       {
+        for (int compteur = 1; compteur <= intNbLimite; compteur++ )
+        {
+           
+           int resultat = intTable * compteur;
+           System.out.println(intTable + " * " + compteur + " = " + resultat);
+            
+        }
+       } else {
+           System.out.println("Le nombre limite est inférieur à 0. Il ne doit pas être négatif !");
+       }
         /* 
          * Ecrire la fonction tableDeMultiplication qui va afficher la table
          * intTable entrée en argument.
@@ -78,8 +103,18 @@ public class TP_0_Function {
     }
     
     public static int conversionBaseDix(int intNbrBinaireConvertir)
-    {
-    
+    {   
+        // Il faut créer un tableau et mettre le nombre intNbrBinaireConvertir puis séparer par une virgule les chiffres du nombre,
+        // et selectionner chaque chiffre avec intNbrBinaireConvertir[0] etc , enfin faire le calcul ci dessous.
+        int resultat = 0;
+        for (int compteur = 0; compteur <= 7; compteur++ )
+        {   
+            int reste = intNbrBinaireConvertir % 2;
+            int total = reste;
+            int calcul = reste*2^compteur;
+            resultat = resultat + calcul;
+        }
+        return(resultat);
         /*
          * Pour cette fonction, il est conseillé de comprendre comment on 
          * convertit un nombre binaire en base 10, veuillez suivre ce lien :
@@ -93,7 +128,8 @@ public class TP_0_Function {
     }
     
     public static String codeCesar(String strMot, int intDecalage) //QUESTION BONUS
-    {
+    {   
+        
         /*
          * Ecrire une fonction qui va permettre de chiffre un mot strMot à 
          * l'aide d'un décalage intDecalage.
